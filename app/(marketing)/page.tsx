dbf7e19cf594757799d4ddb07c3e1ae42b7fb838
Bartlomiej95 <panfilbartlomiej@gmail.com>
2023-11-22 15:30:36 +0100

    Add routing setup

---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,9 @@
+
+export default function HomePage() {
+    return (
+      <main className="flex px-0 min-h-screen flex-col items-center mx-[20px] justify-between p-24">
+        Hello from Pocketsub
+      </main>
+    )
+  }
+  
